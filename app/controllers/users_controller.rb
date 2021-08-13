895062435b59f7b32536cb3bb4b0bdac94e5418c
Christian Doucette <cdoucette@uchicago.edu>
2021-08-13 18:07:41 -0400

    Added token refreshing when it is expired

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -114,7 +114,17 @@ class UsersController < ApplicationController
 	puts 'This is an activity creation: will attempt to automatically change the name'
 	# potentially should make it only change if the name is one of the default ones, so custom names won't be overriden
 #	this_user = User.find(params['owner_id']
-#	if Time.now < this_user.token_exp_date
+	if Time.now < this_user.token_exp_date
+		puts "Updating out of date user token (#{this_user.token_exp_date} when current time is #{Time.now}"
+		response = @client.oauth_token(refresh_token: this_user.refresh_token, grant_type: 'refresh_token')
+		this_user.update(
+			:access_token   => response.access_token,
+			:refresh_token  => response.refresh_token,
+			:token_exp_date => response.expires_at
+		)
+		puts "Just updated user, new info is #{response}"
+		
+	end
         render json: {}, status: :ok
       else
         render json: {}, status: :ok
