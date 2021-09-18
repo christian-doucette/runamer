ce7dcbb433547ee11faa760c2aced16129f7bf3a
Christian Doucette <cdoucette@uchicago.edu>
2021-09-17 21:37:02 -0400

    Switched references to own object in user model from this_user to self

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
 	def update_user_token!(client)
-		if Time.now.to_i >= this_user.token_exp_date
-			puts "Updating out of date user token (#{this_user.token_exp_date} when current time is #{Time.now}"
-			response = client.oauth_token(refresh_token: this_user.refresh_token, grant_type: 'refresh_token')
-			this_user.update(
+		if Time.now.to_i >= self.token_exp_date
+			puts "Updating out of date user token (#{self.token_exp_date} when current time is #{Time.now}"
+			response = client.oauth_token(refresh_token: self.refresh_token, grant_type: 'refresh_token')
+			self.update(
 				:access_token   => response.access_token,
 				:refresh_token  => response.refresh_token,
 				:token_exp_date => response.expires_at
